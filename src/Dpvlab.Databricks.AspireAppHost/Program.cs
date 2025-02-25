2c2ec3385cf7c1b1faf425d973d038403151559d
David Vreony <dpvreony@users.noreply.github.com>
2025-02-25 19:58:39 +0000

    initial spike

---
+++ src/Dpvlab.Databricks.AspireAppHost/Program.cs
@@ -0,0 +1,85 @@
+﻿// Copyright (c) 2022 DHGMS Solutions and Contributors. All rights reserved.
+// This file is licensed to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading.Tasks;
+using Aspire.Hosting;
+
+namespace Dpvlab.Databricks.AspireAppHost
+{
+    /// <summary>
+    /// Program entry point for the application.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// Main entry point for the application.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static async Task Main(string[] args)
+        {
+            var app = GetApplication(args);
+            await app.RunAsync();
+        }
+
+        /// <summary>
+        /// Gets the builder for the distributed application.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>Instance of application builder.</returns>
+        public static IDistributedApplicationBuilder GetBuilder(string[] args)
+        {
+            var builder = DistributedApplication.CreateBuilder(args);
+
+            //builder.AddContainer("spark", "apache/spark").WithEntrypoint("/opt/spark/bin/spark-shell");
+
+            //builder.AddContainer("spark", "jupyter/pyspark-notebook").WithHttpEndpoint(8888, 8888);
+
+            const string masterHostName = "spark-master";
+            const string imageName = "bitnami/spark";
+
+            var sparkContainer = builder.AddContainer(
+                    masterHostName,
+                    imageName)
+                .WithEnvironment("SPARK_MODE", "master")
+                .WithEnvironment("SPARK_MASTER_HOST", "0.0.0.0")
+                .WithEnvironment("SPARK_MASTER_PORT", "7077")
+                .WithEndpoint(8080, 8080, "http", name: "web-ui")
+                .WithEndpoint(7077, 7077, name: "spark-master")
+                .WithEndpoint(4040, 4040, name: "spark-app-ui");
+
+            var sparkMasterUri = new UriBuilder
+            {
+                Scheme = "spark",
+                Host = masterHostName,
+                Port = 7077
+            };
+
+            var sparkWorker = builder.AddContainer(
+                    "spark-worker",
+                    imageName)
+                .WithEnvironment("SPARK_MODE", "worker")
+                .WithEnvironment("SPARK_MASTER_URL", sparkMasterUri.ToString)
+                .WithEndpoint(8081, 8081)
+                .WaitFor(sparkContainer);
+
+            builder.Build().Run();
+
+            return builder;
+        }
+
+        /// <summary>
+        /// Gets the distributed application.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        /// <returns>Instance of the application.</returns>
+        public static DistributedApplication GetApplication(string[] args)
+        {
+            var builder = GetBuilder(args);
+            var app = builder.Build();
+            return app;
+        }
+    }
+}
